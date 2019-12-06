ac0c28d70ab9a39153ac63ea33e57801df6d8582
rits1272 <ritikjain1272@gmail.com>
2019-12-07 04:38:14 +0530

    Fix : Added verbose_plural_name for the models

--- main/models.py
+++ main/models.py
@@ -24,13 +24,14 @@ class Rooms(models.Model):
     guesthouse = models.ForeignKey(GuestHouse, on_delete=models.CASCADE, null=False, blank=False)
     price = models.PositiveIntegerField( default=None, blank=False, null=False)
     services = models.CharField( default="No special services available", null=True, max_length=50)
-			
+            
     class Meta:
+        verbose_name_plural='Rooms'
         db_table = 'OGHBS_Rooms'
         ordering = ['roomID']
-	
+    
     def __str__(self):
-    	return self.roomID
+        return self.roomID
 
 class Reservation(models.Model):
     bookingID = models.CharField( default=None, max_length=30)
@@ -52,13 +53,14 @@ class Reservation(models.Model):
     ]
     room_type = models.CharField(max_length=20, choices=ROOM_TYPES, null=True, blank=True)
     booktime = models.DateField(null=False)
-				
+                
     class Meta:
-    	db_table = 'OGHBS_Reservation'
-    	ordering = ['-booktime']
-				
+        verbose_name_plural='Reservation'
+        db_table = 'OGHBS_Reservation'
+        ordering = ['-booktime']
+                
     def __str__(self):
-    	return self.bookingID
+        return self.bookingID
 
 class PreReservation(models.Model):
     start_date = models.DateField(null=True, blank=True)
@@ -77,34 +79,34 @@ class PreReservation(models.Model):
         return  str(self.guesthouse) + ' || ' + str(self.start_date) + ' - ' + str(self.end_date)
 
 class Payment(models.Model):
-	paymentID = models.CharField( default=None, max_length=30)			
-	amount = models.PositiveIntegerField( default=None, blank=False, null=False)
-	reservation = models.ForeignKey('Reservation', on_delete=models.CASCADE, related_name='payments')
-	user_booked = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
-	payment_time = models.DateField( default=timezone.now, null=False)
-				
-	class Meta:
-		db_table = 'OGHBS_Payments'
-		ordering = ['-payment_time']
-				
-	def __str__(self):
-		return self.paymentID
+    paymentID = models.CharField( default=None, max_length=30)			
+    amount = models.PositiveIntegerField( default=None, blank=False, null=False)
+    reservation = models.ForeignKey('Reservation', on_delete=models.CASCADE, related_name='payments')
+    user_booked = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    payment_time = models.DateField( default=timezone.now, null=False)
+                
+    class Meta:
+        db_table = 'OGHBS_Payments'
+        ordering = ['-payment_time']
+                
+    def __str__(self):
+        return self.paymentID
 
 class Refund(models.Model):
 
-	refundID = models.CharField( default=None, max_length=30)
-	payment = models.ForeignKey( 'Payment', on_delete=models.CASCADE)
-	reservation = models.ForeignKey( 'Reservation', on_delete=models.CASCADE, related_name='refunds')
-	amount = models.PositiveIntegerField( default=None, blank=False, null=False)
-	user_booked = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
-	refund_time = models.DateTimeField( null=False)
-				
-	class Meta:
-		db_table = 'OGHBS_Refunds'
-		ordering = ['-refund_time']
-				
-	def __str__(self):
-		return self.refundID        
+    refundID = models.CharField( default=None, max_length=30)
+    payment = models.ForeignKey( 'Payment', on_delete=models.CASCADE)
+    reservation = models.ForeignKey( 'Reservation', on_delete=models.CASCADE, related_name='refunds')
+    amount = models.PositiveIntegerField( default=None, blank=False, null=False)
+    user_booked = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    refund_time = models.DateTimeField( null=False)
+                
+    class Meta:
+        db_table = 'OGHBS_Refunds'
+        ordering = ['-refund_time']
+                
+    def __str__(self):
+        return self.refundID        
 
 class GuestDetails(models.Model):
     first_name = models.CharField(max_length=100, null=True, blank=True)
@@ -112,6 +114,9 @@ class GuestDetails(models.Model):
     phone = models.CharField(max_length=100, null=True, blank=True)
     email = models.CharField(max_length=100, null=True, blank=True)
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural='GuestDetails'
 
     def __str__(self):
         return self.first_name
@@ -123,7 +128,7 @@ class Feedback(models.Model):
     user_of = models.ForeignKey( User,on_delete=models.DO_NOTHING, null=False, blank=False)			
     time = models.DateField( default=timezone.now, null=False)
     feed = models.TextField( default=None)
-				
+                
     class Meta:
         db_table = 'OGHBS_feedbacks'
         ordering = ['-time']
@@ -137,8 +142,11 @@ class WaitingOn(models.Model):
     date_booked = models.DateField(default=timezone.now, null=False)     
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)  
-	
-	
+
+    class Meta:
+        verbose_name_plural='WaitingOn'
+    
+    
 #class Message(models.Model):
 #	messageID = models.CharField(null=True)
 #	staff = models.ForeignKey(User, on_delete=models.CASCADE)
